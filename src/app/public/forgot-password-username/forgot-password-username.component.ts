7ec61272c7521d018728114e1d0439f424830d23
Kishan Vishwakarma <kishanv10090@gmail.com>
2024-08-31 11:16:17 +0530

    added bootstrap and components

---
+++ src/app/public/forgot-password-username/forgot-password-username.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password-username',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgot-password-username.component.html',
+  styleUrl: './forgot-password-username.component.scss'
+})
+export class ForgotPasswordUsernameComponent {
+
+}
